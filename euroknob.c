db3f5e51f0a0008210468deefb94e3444c815c5c
mitxela <mitxela@mitxela.com>
2025-04-10 16:41:05 +0100

    read ADC input too

--- euroknob.c
+++ euroknob.c
@@ -10,6 +10,28 @@
 // ADC PA1
 // LEDs on PD6,PD5,PD4,PD3,PD2, PC7,PC6,PC5,PC4,PC3
 
+void init_bargraph()
+{
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOD;
+
+	GPIOC->CFGLR &= ~((0xf<<(4*7)) | (0xf<<(4*6)) | (0xf<<(4*5)) | (0xf<<(4*4)) | (0xf<<(4*3)));
+	GPIOC->CFGLR |=((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*7))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*5))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*3));
+
+	GPIOD->CFGLR &= ~((0xf<<(4*6)) | (0xf<<(4*5)) | (0xf<<(4*4)) | (0xf<<(4*3)) | (0xf<<(4*2)));
+	GPIOD->CFGLR |=((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*5))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*3))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*2));
+
+	GPIOD->OUTDR |= 0xFF;
+	GPIOC->OUTDR |= 0xFF;
+}
+
 void draw_bargraph(uint16_t v)
 {
 	uint8_t outD = 0xFF, outC=0xFF;
@@ -31,28 +53,47 @@ void draw_bargraph(uint16_t v)
 	GPIOC->OUTDR |= 0xFF;
 }
 
+void init_adc()
+{
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_ADC1;
+
+	// PA1 is analog input chl 1
+	GPIOA->CFGLR &= ~(0xf<<(4*1));	// CNF = 00: Analog, MODE = 00: Input
+
+	RCC->APB2PRSTR |= RCC_APB2Periph_ADC1;
+	RCC->APB2PRSTR &= ~RCC_APB2Periph_ADC1;
+
+	RCC->CFGR0 &= ~(0x1F<<11); //RCC_ADCPRE = 0
+	ADC1->RSQR1 = 0;
+	ADC1->RSQR2 = 0;
+	ADC1->RSQR3 = 1;
+	// sampling time
+	ADC1->SAMPTR2 &= ~(ADC_SMP0<<(3*7));
+	ADC1->SAMPTR2 |= 7<<(3*7);
+
+	// turn on, set sw trig
+	ADC1->CTLR2 |= ADC_ADON | ADC_EXTSEL;
+	// reset calibration
+	ADC1->CTLR2 |= ADC_RSTCAL;
+	while(ADC1->CTLR2 & ADC_RSTCAL);
+	// calibrate
+	ADC1->CTLR2 |= ADC_CAL;
+	while(ADC1->CTLR2 & ADC_CAL);
+}
+
+uint16_t read_adc(void)
+{
+	ADC1->CTLR2 |= ADC_SWSTART;
+	while(!(ADC1->STATR & ADC_EOC));
+	return ADC1->RDATAR;
+}
+
 int main()
 {
 	SystemInit();
 
-	RCC->APB2PCENR |= RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOD;
-
-	GPIOC->CFGLR &= ~((0xf<<(4*7)) | (0xf<<(4*6)) | (0xf<<(4*5)) | (0xf<<(4*4)) | (0xf<<(4*3)));
-	GPIOC->CFGLR |=((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*7))
-				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6))
-				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*5))
-				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4))
-				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*3));
-
-	GPIOD->CFGLR &= ~((0xf<<(4*6)) | (0xf<<(4*5)) | (0xf<<(4*4)) | (0xf<<(4*3)) | (0xf<<(4*2)));
-	GPIOD->CFGLR |=((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6))
-				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*5))
-				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4))
-				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*3))
-				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*2));
-
-	GPIOD->OUTDR |= 0xFF;
-	GPIOC->OUTDR |= 0xFF;
+	init_bargraph();
+	init_adc();
 
 	if (i2c_init(I2C_CLK_100KHZ) != I2C_OK) printf("Failed to init I2C");
 	Delay_Ms(100);
@@ -67,9 +108,14 @@ int main()
 		if (i2c_read(AS5600_ADDR, AS5600_MAGNITUDE, buf, 2) == I2C_OK)
 			magnitude = (buf[0]<<8)+ buf[1];
 
-		draw_bargraph(raw_angle);
+		uint16_t raw_adc = read_adc();
 
-		//printf("Read %04d / %04d \n", raw_angle, magnitude);
+		if (magnitude > 500) { // magnitude is about 2000 when plugged in
+			draw_bargraph(raw_angle);
+		} else {
+			draw_bargraph(raw_adc*4);
+		}
+		//printf("Read %04d / %04d / %04d \n", raw_angle, magnitude, raw_adc);
 
 		Delay_Ms(10);
 	}
